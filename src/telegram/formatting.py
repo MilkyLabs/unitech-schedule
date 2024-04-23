ea9b718a2d12fbcc1215525065d1df8c1d2a16a2
Katy248 <petrovanton247@gmail.com>
2024-04-23 20:02:24 +0300

    Add emoji formatting

--- src/telegram/formatting.py
+++ src/telegram/formatting.py
@@ -1,6 +1,7 @@
 import logging
 from aiogram import html
 import dateutil
+import re as regex
 from datetime import date
 
 import dateutil.parser
@@ -15,12 +16,26 @@ DAYS = {
     6: "Вс",
 }
 
+SUBJECT_EMOJI ={
+    "[Мм]атем": "🤮"
+}
+
+def format_subject(subject: str) -> str:
+    emoji = ""
+    for key in SUBJECT_EMOJI.keys():
+        r = regex.compile(key)
+        if (r.match(subject)):
+            emoji = SUBJECT_EMOJI[key]
+            break
+    return f"{subject} {emoji}" 
+
+
 def format_day_time_table(time_table) -> str:
     logging.info(time_table['date'])
     weekday = DAYS[dateutil.parser.parse(time_table['date']).weekday()]
-    message = f"Расписание на {html.bold(time_table['date'])} ({weekday})\n\n"
+    message = f"🗓 Расписание на {html.bold(time_table['date'])} ({weekday})\n\n"
 
     for lesson in time_table['lessons']:
-        message += f"* {lesson['number']}-я пара {lesson['subject']} {html.italic(lesson['room'] + ' каб.')}\n"
+        message += f"🔘 {lesson['number']}-я пара {format_subject(lesson['subject'])} {html.italic(lesson['room'] + ' каб.')}\n"
 
     return message
